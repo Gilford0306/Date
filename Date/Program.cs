11fe8263ffe498860968fd52c9c4b1a9bd0b7fdd
Gilford0306 <Gilford0306@gmail.com>
2022-06-16 13:06:28 +0300

    Add project files.

---
+++ Date/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задание 5
+//Пользователь вводит с клавиатуры дату. Приложе-
+//ние должно отобразить название сезона и дня недели. 
+//Например, если введено 22.12.2021, приложение должно 
+//отобразить Winter Wednesday.
+
+int k;
+do
+{
+    Console.WriteLine("Input date (template - 22.12.2021) ");
+    DateTime dtu = DateTime.Parse(Console.ReadLine());
+    int mon = (dtu.Month);
+    string seaz;
+    if (mon > 11 || mon < 3)
+    {
+        seaz = "Winter";
+    }
+    else if (mon > 2 && mon < 6)
+    {
+        seaz = "Spring";
+    }
+    else if (mon > 5 && mon < 9)
+    {
+        seaz = "Summer";
+    }
+    else
+    {
+        seaz = "Fall";
+    }
+    Console.WriteLine(dtu.DayOfWeek+ " - " + seaz);
+    Console.WriteLine("Int key - 1 - to continue, 0 - to exit");
+    k = int.Parse(Console.ReadLine());
+} while (k == 1);
+return;
